8b107e04535af75b9dcd1f2929724525e7728709
ar00ster18 <arielarusy12@gmail.com>
2024-11-17 18:12:41 +0200

    Finished the code

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,37 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String s = args[0].toUpperCase();
+            
+            int reps = Integer.parseInt(args[1]);
+
+            for(int i = 0; i < s.length(); i++){
+
+                String word = "a ";
+
+                if(s.charAt(i) == 'A' || 
+                s.charAt(i) == 'E' || 
+                s.charAt(i) == 'F' || 
+                s.charAt(i) == 'H' || 
+                s.charAt(i) == 'I' || 
+                s.charAt(i) == 'L' ||
+                s.charAt(i) == 'M' ||
+                s.charAt(i) == 'N' ||
+                s.charAt(i) == 'O' ||
+                s.charAt(i) == 'R' ||
+                s.charAt(i) == 'S' ||
+                s.charAt(i) == 'X'){
+                        word = "an";
+                }
+                System.out.println("Give me " + word + " " + s.charAt(i) + ": " + s.charAt(i) + "!");
+
+            }
+
+            System.out.println("What does that spell?");
+
+            for(int i = 0; i < reps; i++){
+                System.out.println(s.toUpperCase() + "!!!");
+            }
+
         }
 }
